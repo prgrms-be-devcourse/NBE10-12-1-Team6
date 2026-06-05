78f57d2f5e720ddc539abd12681a0a78679c20b4
bulmyung <bulmyun9@gmail.com>
2026-06-05 14:27:19 +0900

    refactor: 상품Entity BaseEntity 상속으로 수정

--- backend/src/main/java/com/back/domain/product/product/entity/Product.java
+++ backend/src/main/java/com/back/domain/product/product/entity/Product.java
@@ -1,22 +1,16 @@
 package com.back.domain.product.product.entity;
 
+import com.back.global.jpa.entity.BaseEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
 import lombok.*;
-
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-public class Product {
-    @Id
-    @GeneratedValue(strategy = IDENTITY)
-    private long id;
+public class Product extends BaseEntity {
     private String name;
     private int price;
     @Column(columnDefinition = "TEXT")
